c114e9c1c7615f22f86cf23e70b51c7ec8637696
King <jerquinbayudo@gmail.com>
2026-01-28 20:45:52 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+if (typeof window !== "undefined") {
+  const { origin, pathname, search, hash, href } = window.location;
+  if (pathname === "/" && href.endsWith("/")) {
+    window.history.replaceState(null, "", `${origin}${search}${hash}`);
+  }
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
