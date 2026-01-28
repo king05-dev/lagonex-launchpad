8b317b6ce0bc36b56c9c895c230c4d937bc5b0d9
King <jerquinbayudo@gmail.com>
2026-01-28 20:47:45 +0800

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,19 @@ import "./index.css";
 
 if (typeof window !== "undefined") {
   const { origin, pathname, search, hash, href } = window.location;
-  if (pathname === "/" && href.endsWith("/")) {
-    window.history.replaceState(null, "", `${origin}${search}${hash}`);
+  const decodedPathname = (() => {
+    try {
+      return decodeURIComponent(pathname);
+    } catch {
+      return pathname;
+    }
+  })();
+
+  const isRootLikePath = decodedPathname.replace(/[\s\u00A0]+/g, "") === "/";
+  if ((isRootLikePath && href.endsWith("/")) || decodedPathname !== "/") {
+    if (isRootLikePath) {
+      window.history.replaceState(null, "", `${origin}${search}${hash}`);
+    }
   }
 }
 
