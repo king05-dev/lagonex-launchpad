730a1324463b8b86935fed8babfb13a00f84311b
King <jerquinbayudo@gmail.com>
2026-01-29 21:15:39 +0800

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,10 @@ if (typeof window !== "undefined") {
   })();
 
   const isRootLikePath = decodedPathname.replace(/[\s\u00A0]+/g, "") === "/";
-  if (isRootLikePath && (decodedPathname !== "/" || href.endsWith("/"))) {
-    window.history.replaceState(null, "", `${origin}${search}${hash}`);
+ if ((isRootLikePath && href.endsWith("/")) || decodedPathname !== "/") {
+    if (isRootLikePath) {
+      window.history.replaceState(null, "", `${origin}${search}${hash}`);
+    }
   }
 }
 
